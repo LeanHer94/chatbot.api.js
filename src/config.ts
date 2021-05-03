70fdb81ce2049d4c1c8ea74ffbfe58ddd680ba7b
Leandro Herenu <lherenu@coalfire.com>
2021-05-03 15:49:21 -0300

    adding db

--- src/config.ts
+++ src/config.ts
@@ -7,11 +7,13 @@ const worldtimeapi = process.env.TIME_API
 const apiCacheTime = process.env.API_CACHE_TIME
 const apiRetry = process.env.API_RETRY
 const apiRetryTime = process.env.API_RETRY_MILLISECONDS
+const database = process.env.DATABASE_URL;
 
 export {
     serverPort,
     worldtimeapi,
     apiCacheTime,
     apiRetry,
-    apiRetryTime
+    apiRetryTime,
+    database
 }
